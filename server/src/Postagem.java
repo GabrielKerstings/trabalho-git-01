b886ec046449c5963687947dbe1ed87078af5e18
eduardomeireles <eduardode.meireles@gmail.com>
2022-05-11 16:28:31 -0300

    feat: classe Postagem

---
+++ server/src/Postagem.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Postagem {
+
+
+    public Usuario donoPostagem;
+    public Date dataHora;
+    public String conteudo;
+    public ArrayList[Comentario] comentariosPostagem = new ArrayList[100];
+
+}
